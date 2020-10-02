4ca5ac6bf16a2e07aeac91b62bc30e1f33efb5b1
Nii Addo <ankamahgodson@yahoo.com>
2020-10-02 16:26:44 +0000

    removed build from gitignore

---
+++ build/precache-manifest.2405a0d02f0aca8dfdd2d5b9fef7698f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52e794d61a72298c1768506cc85bbcb9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "764298ce0ac8b121f067",
+    "url": "/static/css/main.a9062ba4.chunk.css"
+  },
+  {
+    "revision": "7b8e392e97de22ed4f88",
+    "url": "/static/js/2.47d2c85c.chunk.js"
+  },
+  {
+    "revision": "77e7f328dacc981fca681ee4ae94d669",
+    "url": "/static/js/2.47d2c85c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "764298ce0ac8b121f067",
+    "url": "/static/js/main.0ee5a8db.chunk.js"
+  },
+  {
+    "revision": "049b6ba031ca4f437807",
+    "url": "/static/js/runtime-main.2490ea1a.js"
+  }
+]);
